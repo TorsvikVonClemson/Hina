f1bc1a92b8587ac808a4fecf41da990b9230b1ad
TorsvikVonClemson <savagearmitage@aol.com>
2017-06-22 18:36:42 -0700

    Update pdfwriter.py

--- Hina/generators/adnd/charactersheet/pdfwriter.py
+++ Hina/generators/adnd/charactersheet/pdfwriter.py
@@ -1,8 +1,19 @@
 from reportlab.pdfgen import canvas
+from reportlab.pdfgen import inch
+
+    #c.line(x1,y1,x2,y2)
+
 
 def write(w,fp):
     c=canvas.Canvas(fp)
-    c.drawString(100,100,w)
-    c.showPage()
-    c.save()
+    c.translate(inch,inch) #moves origin from bottom left to top left
+    c.setFont("Helvetica",10)
+    
+    c.drawString(0,0,"Character")
+    c.line(.5*inch,0,4*inch,0)
+    c.drawString(1*inch,0)
+    
+    
+    c.showPage() #finishes writing on current page, starts a new page if more is added
+    c.save() #finalizes the document
     return 0
